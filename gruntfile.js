00cc855d2f3d3ff0ee2e717499154117482a91c4
Alex Sohail <elementrat@gmail.com>
2014-02-08 00:26:28 -0600

    changes

--- gruntfile.js
+++ gruntfile.js
@@ -2,12 +2,13 @@ module.exports = function (grunt) {
   grunt.initConfig({
   	watch : {
   		all : {
-  			files : ["**/**/*.js","**/**/*.html"],
-  			options : {livereload:true}
-  		}
-  	}
-  });
+  			files : ["**/**/*.js","**/**/*.html", "**/**/*.css"],
+  			options : { livereload : true }
+  		},
+    }
+  }); 
 
+  grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks("grunt-contrib-watch")
   grunt.registerTask('default', ['watch']);
 }
