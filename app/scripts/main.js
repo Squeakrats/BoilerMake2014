57d537e2a6c491f36e7938a9f299edd3e1cfb244
Alex Sohail <elementrat@gmail.com>
2014-02-08 22:24:36 -0600

    visualizer live

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,201 +1,51 @@
-
-var margin = {top: 20, right: 120, bottom: 20, left: 120},
-    width = 960 - margin.right - margin.left,
-    height = 800 - margin.top - margin.bottom;
-    
-var i = 0,
-    duration = 250;
-    root;
-
-var tree = d3.layout.tree()
-    .size([height, width]);
-
-var diagonal = d3.svg.diagonal()
-    .projection(function(d) { return [d.y, d.x]; });
-
-var svg = d3.select(".data-tree").append("svg")
-    .attr("width", width + margin.right + margin.left)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-  flare = {
-    "name": "ROOT",
-    "children": []
-  }
-
-  root = flare;
-  root.x0 = height / 2;
-  root.y0 = 0;
-
-  function collapse(d) {
-    if (d.children) {
-      d._children = d.children;
-      d._children.forEach(collapse);
-      d.children = null;
-    }
-  }
-
-  root.children.forEach(collapse);
-  update(root);
-
-
-d3.select(self.frameElement).style("height", "800px");
-
-function update(source) {
-
-  // Compute the new tree layout.
-  var nodes = tree.nodes(source).reverse();
-      links = tree.links(nodes);
-
-  // Normalize for fixed-depth.
-  nodes.forEach(function(d) { d.y = d.depth * 180; });
-
-  // Update the nodes…
-  var node = svg.selectAll("g.node")
-      .data(nodes, function(d) { return d.id || (d.id = ++i); });
-
-  // Enter any new nodes at the parent's previous position.
-  var nodeEnter = node.enter().append("g")
-      .attr("class", "node")
-      .attr("transform", function(d) { return "translate(" + source.y0 + "," + source.x0 + ")"; })
-      .on("click", click);
-
-  nodeEnter.append("circle")
-      .attr("r", 1e-6)
-      .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
-
-  nodeEnter.append("text")
-      .attr("x", function(d) { return d.children || d._children ? -10 : 10; })
-      .attr("dy", ".35em")
-      .attr("text-anchor", function(d) { return d.children || d._children ? "end" : "start"; })
-      .text(function(d) { return d.name; })
-      .style("fill-opacity", 1e-6);
-
-  // Transition nodes to their new position.
-  var nodeUpdate = node.transition()
-      .duration(duration)
-      .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
-
-  nodeUpdate.select("circle")
-      .attr("r", 12)
-      .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
-
-  nodeUpdate.select("text")
-      .style("fill-opacity", 1);
-
-  // Transition exiting nodes to the parent's new position.
-  var nodeExit = node.exit().transition()
-      .duration(duration)
-      .attr("transform", function(d) { return "translate(" + source.y + "," + source.x + ")"; })
-      .remove();
-
-  nodeExit.select("circle")
-      .attr("r", 1e-6);
-
-  nodeExit.select("text")
-      .style("fill-opacity", 1e-6);
-
-  // Update the links…fduration
-  var link = svg.selectAll("path.link")
-      .data(links, function(d) { return d.target.id; });
-
-  // Enter any new links at the parent's previous position.
-  link.enter().insert("path", "g")
-      .attr("class", "link")
-      .attr("d", function(d) {
-        var o = {x: source.x0, y: source.y0};
-        return diagonal({source: o, target: o});
-      });
-
-  // Transition links to their new position.
-  link.transition()
-      .duration(duration)
-      .attr("d", diagonal);
-
-  // Transition exiting nodes to the parent's new position.
-  link.exit().transition()
-      .duration(duration)
-      .attr("d", function(d) {
-        var o = {x: source.x, y: source.y};
-        return diagonal({source: o, target: o});
-      })
-      .remove();
-
-  // Stash the old positions for transition.
-  nodes.forEach(function(d) {
-    d.x0 = d.x;
-    d.y0 = d.y;
-  });
-}
-
-// Toggle children on click.
-function click(d) {
-  if (d.children) {
-    d._children = d.children;
-    d.children = null;
-  } else {
-    d.children = d._children;
-    d._children = null;
-  }
-  update(d);
-}
-
-
-
-
-
-
-
 var TEST_URL = "http://www.learnyouahaskell.com";
-var socket = {}
-var root   = {}
-var requested = false;
 
+var socket = {}
+var requested = false;
+var root = {}; //the d3 root vis
+//
 var requestSite = function(){
+  //prevent accidentally requesting multiple times.
   if(!requested){
     requested = true;
-  socket = io.connect('ws://localhost:1338')
-
-  socket.on('connect', function () {
-    socket.emit('get:structure:init',{ url:TEST_URL })
+    socket = io.connect('ws://localhost:1338')
+    socket.on('connect', function () {
+      socket.emit('get:structure:init', { url:TEST_URL })
   })
   socket.on('complete', function(data){
+    console.log("Data complete from server")
     var zip  = new JSZip()
-    console.log(data[0])
+    console.log(data)
     for(var i=0;i<data.length;i++){
         var value = data[i]
          if(value.resourceText){
            zip.file(value.path, value.resourceText) 
           }
-
     }
 
-
-    //var content = zip.generate();
-      //location.href="data:application/zip;base64,"+content;
-
- // document.getElementById('data_uri').href = "data:application/zip;base64," + zip.generate();
-
+  //var content = zip.generate();
+  //location.href="data:application/zip;base64,"+content;
+  //document.getElementById('data_uri').href = "data:application/zip;base64," + zip.generate();
   // Blob
   var blobLink = document.createElement("a");
   try {
     blobLink.download = "hello.zip";
     blobLink.href = window.URL.createObjectURL(zip.generate({type:"blob"}));
-    blobLink.innerHTML = "asdASDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD"
-
-    console.log('adsasd')
+    blobLink.innerHTML = "Click to Download!"
   } catch(e) {
     blobLink.innerHTML += " (not supported on this browser)";
     console.log("awww")
   }
-    $(".data-tree").append(blobLink)
->>>>>>> 1bd57611d684a7e517af277dc0906d940ecc745b
-
+    $(".footer").append(blobLink)
   })
   socket.on('reply:structure:update' , function (data){
-    findHome(data) 
-   update(root)
+
+       findHome(data)
+       //console.log(root)
+       console.log("pre")
+       updateVis(root)
+      //update(root)
+  // update(root)
    /*
 
 $.getJSON(addthese.json, function(addTheseJSON) {
@@ -205,23 +55,26 @@ $.getJSON(addthese.json, function(addTheseJSON) {
 });
 
    */
-    console.log(JSON.stringify(root))
+
   })
 }
 }
 
+//Finds the appropriate place in the tree for a node
 function findHome(data){
+//If this is the node doesn't have a referrer, its a root node
   if(!data.referrer){
     root.name = data.name
     root.children = [] // {name:data.name,children:[]}
     //addNode()
     console.log("setup root")
   }else{
-    var node = findNode(data.referrer,root)
+    //get the appropriate parent node
+    var node = findParentNode(data.referrer,root)
     if(node){
-      console.log(node)
+      //console.log("we found a parent!" + node)
       node.children.push({
-        name:data.name,
+        name:JSON.stringify(data.name),
         children:[]
       })
     }
@@ -229,13 +82,13 @@ function findHome(data){
   }
 }
 
-function findNode(referrer, node){
+function findParentNode(referrer, node){
   if(node.name == referrer){
     return node
   }
 
   for(var i = 0; i < node.children.length;i ++){
-    var foundNode = findNode(referrer,node.children[i])
+    var foundNode = findParentNode(referrer,node.children[i])
       if(foundNode) return foundNode
   }
 
